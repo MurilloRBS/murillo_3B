1d83a4f737e7063e674699509b91fadd904660f7
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-03-15 10:50:01 -0300

    e

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
 
 
 for (let i = 0; i < botoes.length; i++) {
@@ -15,10 +19,5 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
