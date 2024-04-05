8bdab2cbe5761aa68becd5c631412a969910d43d
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-04-05 10:32:35 -0300

    u

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function atualizaCronometro(){
 function atualizaCronometro(){
        
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);   
+        contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
