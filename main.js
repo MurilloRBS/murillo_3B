de710d9ef22f201c029dbf07adda2ac7c7590274
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-04-05 10:08:28 -0300

    e4

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
 
 
 for (let i = 0; i < botoes.length; i++) {
@@ -37,6 +35,19 @@ function calculaTempo(tempoObjetivo) {
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
 }
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+
+
 
 
 
