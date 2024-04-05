c3bdc06fa6ed4c52a08c16ab70a92d3c49f037b4
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-04-05 10:09:53 -0300

    e

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
