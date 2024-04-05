25df81669e07629917ff8e0f798c2ef9c0ff0444
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-04-05 10:30:19 -0300

    u

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
